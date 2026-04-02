7163bbdb651e26bbe9546515b16f5ee22bfaa529
Ferdinand Couture <fcouturemobile@gmail.com>
2026-04-02 10:27:18 -0400

    Update from Hostinger export

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// Build: 2026-04-02T10:18 EDT - members debug
+// Build: 2026-04-02T10:24 EDT - x-tenant in fetch
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 process.env.NODE_ENV = "production";
